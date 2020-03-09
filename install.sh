1253c9c70014355c648c9597253b62175d7335e7
Robert Wilbrandt <uudvo@student.kit.edu>
2020-03-09 13:14:44 +0100

    Added scripts from fmauchs dot_profile

--- install.sh
+++ install.sh
@@ -1,5 +1,40 @@
 #!/bin/sh
 
-for dir in */ ; do
-	stow $dir
+
+# Make sure git directory exists
+if [ ! -d "git" ] ;
+then
+  mkdir git
+fi
+
+rm -rf profile_plugins
+mkdir -p profile_plugins/.profile_plugins
+extra_plugins_file="$(pwd)/profile_plugins/.profile_plugins/extra_plugins.sh"
+
+echo "Updating dependencies..."
+
+cd git
+
+if [ -d "dot_profile" ] ;
+then
+  cd dot_profile
+  git pull
+  cd ..
+else
+  git clone https://github.com/fmauch/dot_profile.git
+fi
+
+echo "source $(pwd)/dot_profile/.profile_plugins/ros_helper_functions.sh" >> "$extra_plugins_file"
+echo "source $(pwd)/dot_profile/.profile_plugins/git_batch_functions.sh" >> "$extra_plugins_file"
+
+cd ..
+
+echo "Stowing configuration..."
+
+stow_dirs="shell profile_plugins"
+
+for dir in "$stow_dirs" ; do
+  stow $dir
 done
+
+echo "Done"
